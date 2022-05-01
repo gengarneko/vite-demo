533922891769b7cb27ffd1cc799a018bab12d5bc
GengarNeko <gengarneko@gmail.com>
2022-05-01 22:57:11 +0800

    feat: init repo
    Author:    GengarNekoge <gengarneko@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Counter, Counter2 } from './jotai/Demo';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+      <Counter2 />
+      <button>showModal</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/toast/useToast.ts
@@ -0,0 +1,22 @@
+// import React from 'react'
+//
+// // @ts-ignore
+// const ToastContext = React.createContext();
+//
+// const createStandaloneToast = () => {
+//   const toastImpl = (options?: UseToastOptions) => {
+//     const opts = { ...defaultOptions, ...options } as UseToastOptionsNormalized
+//     opts.position = getToastPlacement(opts.position, theme.direction)
+//
+//     const Message: React.FC<RenderProps> = (props) =>
+//       renderWithProviders(props, opts)
+//
+//     return toast.notify(Message, opts)
+//   }
+// }
+//
+// const
+//
+// export const useToast = () => {}
+
+export const useToast = () => {};
