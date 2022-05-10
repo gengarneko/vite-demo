c8c1c49a4015d8856322be281c5e6dbf2da224c8
GengarNeko <gengarneko@gmail.com>
2022-05-10 09:05:20 +0800

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import type { FC } from 'react';
 import { TabContainer } from './components/TabContainer';
+import { Button } from '@tux-ui/base';
 
 // * --------------------------------------------------------------------------- comp
 
 export const Index: FC = () => {
   return (
     <div>
+      <Button />
       <TabContainer />
     </div>
   );
